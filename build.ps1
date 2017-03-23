f2685326a43df6efe589bbf4d0b164a4cb98e75b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-23 10:55:46 +0000

    Add build script for generating correct lib files.

---
+++ build.ps1
@@ -0,0 +1,17 @@
+$lib = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\Lib.exe"
+$files32 = Get-ChildItem -Path "ffmpeg*-win32-dev/lib/*.def"
+$files64 = Get-ChildItem -Path "ffmpeg*-win64-dev/lib/*.def"
+Foreach ($file in $files32) {
+  $defname = $file.FullName
+  $pos = $defname.LastIndexOf("-")
+  $libname = "$($defname.Substring(0, $pos)).lib"
+  Echo "Regenerating $libname..."
+  & "$lib" /def:"$defname" /out:"$libname" /machine:x86
+}
+Foreach ($file in $files64) {
+  $defname = $file.FullName
+  $pos = $defname.LastIndexOf("-")
+  $libname = "$($defname.Substring(0, $pos)).lib"
+  Echo "Regenerating $libname..."
+  & "$lib" /def:"$defname" /out:"$libname" /machine:x64
+}
