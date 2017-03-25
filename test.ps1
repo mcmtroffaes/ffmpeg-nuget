971a88f028ac1e28aecb7e815edbe9fe3243c025
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-25 15:50:56 +0000

    appveyor: run some actual tests

---
+++ test.ps1
@@ -0,0 +1,8 @@
+& .\decoding_encoding.exe h264
+& .\decoding_encoding.exe mp2
+& .\decoding_encoding.exe mpg
+& .\muxing.exe test_muxing.flac
+& .\muxing.exe test_muxing.wav
+& .\muxing.exe test_muxing.aac
+& .\muxing.exe test_muxing.ogg
+& .\muxing.exe test_muxing.mp4
