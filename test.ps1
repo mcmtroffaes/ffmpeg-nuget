744f85f5929c81b85c07bd225fee8a13c69d04ed
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-25 18:16:37 +0000

    Suppress more output.

--- test.ps1
+++ test.ps1
@@ -1,11 +1,14 @@
-& .\decoding_encoding.exe h264 >$null
+Echo "test: decoding/encoding to test.h264..."
+& .\decoding_encoding.exe h264 >$null 2>&1
 if (!(Test-Path "test.h264")) { $host.SetShouldExit(1) }
-& .\decoding_encoding.exe mp2 >$null
+Echo "test: decoding/encoding to test.mp2..."
+& .\decoding_encoding.exe mp2 >$null 2>&1
 if (!(Test-Path "test.mp2")) { $host.SetShouldExit(1) }
 
 $formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
-  & .\muxing.exe $outfile >$null
+  Echo "test: muxing to $outfile..."
+  & .\muxing.exe $outfile >$null 2>&1
   if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
 }
