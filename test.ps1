7858ca5ead687c2ea989f387e181b481849da879
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-25 17:17:00 +0000

    appveyor: reduce verbosity, make artifact from test results

--- test.ps1
+++ test.ps1
@@ -1,8 +1,8 @@
-& .\decoding_encoding.exe h264
-& .\decoding_encoding.exe mp2
-& .\decoding_encoding.exe mpg
-& .\muxing.exe test_muxing.flac
-& .\muxing.exe test_muxing.wav
-& .\muxing.exe test_muxing.aac
-& .\muxing.exe test_muxing.ogg
-& .\muxing.exe test_muxing.mp4
+& .\decoding_encoding.exe h264 >$null 2>&1
+& .\decoding_encoding.exe mp2 >$null 2>&1
+& .\decoding_encoding.exe mpg >$null 2>&1
+& .\muxing.exe test.flac >$null 2>&1
+& .\muxing.exe test.wav >$null 2>&1
+& .\muxing.exe test.aac >$null 2>&1
+& .\muxing.exe test.ogg >$null 2>&1
+& .\muxing.exe test.mp4 >$null 2>&1
