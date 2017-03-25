7b7acb62f1b7d9646151abfadf9b6e7e82cf5267
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-25 17:44:13 +0000

    appveyor: improve test runner

--- test.ps1
+++ test.ps1
@@ -1,8 +1,11 @@
-& .\decoding_encoding.exe h264 >$null 2>&1
-& .\decoding_encoding.exe mp2 >$null 2>&1
-& .\decoding_encoding.exe mpg >$null 2>&1
-& .\muxing.exe test.flac >$null 2>&1
-& .\muxing.exe test.wav >$null 2>&1
-& .\muxing.exe test.aac >$null 2>&1
-& .\muxing.exe test.ogg >$null 2>&1
-& .\muxing.exe test.mp4 >$null 2>&1
+& .\decoding_encoding.exe h264 >$null
+if (!(Test-Path "test.h264")) { $host.SetShouldExit(1) }
+& .\decoding_encoding.exe mp2 >$null
+if (!(Test-Path "test.mp2")) { $host.SetShouldExit(1) }
+
+$formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
+foreach ($format in $formats) {
+  $outfile = "test-muxing.$format"
+  & .\muxing.exe $outfile >$null
+  if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
+}
