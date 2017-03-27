7addbd0c17942d2c9dd344da242490487090a103
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-27 14:20:20 +0100

    Get new version directly from website.

--- update-version.sh
+++ update-version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "Please supply a new version (YYYYMMDD-HASH)"
-    exit 1
-fi
-
 OLD_VERSION=`sed -n 's|[$]ffmpeg = "ffmpeg-\(.*\)"|\1|p' install.ps1`
 OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Nightly.nuspec`
 
-NEW_VERSION=$1
+NEW_VERSION=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'ffmpeg-\K[0-9]+-[a-z0-9]+' | head -n 1`
 NEW_NUGET_VERSION=${NEW_VERSION:0:8}.0.0
+
+if [ "$OLD_VERSION" == "$NEW_VERSION" ]
+then
+  echo "Already up to date."
+  exit 0
+fi
 
 echo "$OLD_VERSION -> $NEW_VERSION"
 echo "$OLD_NUGET_VERSION -> $NEW_NUGET_VERSION"
