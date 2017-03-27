b86af025be70d14d012b47b039f845f3d755f6ac
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-03-27 14:23:58 +0100

    Also include tag command.

--- update-version.sh
+++ update-version.sh
@@ -19,3 +19,5 @@ find . -type f -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
 find . -type f -exec sed -i "s/$OLD_NUGET_VERSION/$NEW_NUGET_VERSION/g" {} \;
 
 git commit -a -m "Update ffmpeg version to $NEW_VERSION."
+echo "To tag, run:"
+echo "    git tag -a -m \"Tagging version $NEW_NUGET_VERSION\"" $NEW_NUGET_VERSION
