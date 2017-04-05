35f44ae3172ca6ff7bed2186e711e7f4a27afc08
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-04-05 08:45:18 +0100

    Update nuget minor version if need be.

--- update-version.sh
+++ update-version.sh
@@ -4,13 +4,24 @@ OLD_VERSION=`sed -n 's|[$]ffmpeg = "ffmpeg-\(.*\)"|\1|p' install.ps1`
 OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Nightly.nuspec`
 
 NEW_VERSION=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'ffmpeg-\K[0-9]+-[a-z0-9]+' | head -n 1`
-NEW_NUGET_VERSION=${NEW_VERSION:0:8}.0.0
 
 if [ "$OLD_VERSION" == "$NEW_VERSION" ]
 then
   echo "Already up to date."
   exit 0
 fi
+
+NEW_NUGET_VERSION_MINOR=0
+
+while true;
+do
+    NEW_NUGET_VERSION=${NEW_VERSION:0:8}.${NEW_NUGET_VERSION_MINOR}.0
+    if [ "$OLD_NUGET_VERSION" != "$NEW_NUGET_VERSION" ]
+    then
+        break
+    fi
+    ((NEW_NUGET_VERSION_MINOR++))
+done
 
 echo "$OLD_VERSION -> $NEW_VERSION"
 echo "$OLD_NUGET_VERSION -> $NEW_NUGET_VERSION"
