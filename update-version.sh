371b23b0f0bf498bfa4181a101e04071c8f0751a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-04-10 09:10:04 +0100

    Minor improvement in script.

--- update-version.sh
+++ update-version.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 OLD_VERSION=`sed -n 's|[$]ffmpeg = "ffmpeg-\(.*\)"|\1|p' install.ps1`
-OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Nightly.nuspec`
-
 NEW_VERSION=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'ffmpeg-\K[0-9]+-[a-z0-9]+' | head -n 1`
 
 if [ "$OLD_VERSION" == "$NEW_VERSION" ]
@@ -11,6 +9,7 @@ then
   exit 0
 fi
 
+OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Nightly.nuspec`
 OLD_NUGET_VERSION_MAJOR=`expr "${OLD_NUGET_VERSION}" : '^\([0-9]*\)[.]'`
 OLD_NUGET_VERSION_MINOR=`expr "${OLD_NUGET_VERSION}" : '^[0-9]*[.]\([0-9]*\)'`
 NEW_NUGET_VERSION_MAJOR=${NEW_VERSION:0:8}
