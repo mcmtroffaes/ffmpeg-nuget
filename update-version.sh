4d682202c9c652be199efa08eeadf240bb8d54c0
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-10-17 15:48:36 +0100

    Update script for new website layout.

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OLD_VERSION=`sed -n 's|[$]ffmpeg = "ffmpeg-\(.*\)"|\1|p' install.ps1`
-NEW_VERSION=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'ffmpeg-\K[0-9]+-[a-z0-9]+' | head -n 1`
+NEW_VERSION=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'value="\K[0-9]+-[a-z0-9]+' | head -n 1`
 
 if [ "$OLD_VERSION" == "$NEW_VERSION" ]
 then
