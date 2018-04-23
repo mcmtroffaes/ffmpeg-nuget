9fbf968e610944f58ad576d4be520f33e1ce3df8
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-23 10:15:39 +0100

    Echo begin and end of each test to see where problem happens.

--- test/test.ps1
+++ test/test.ps1
@@ -1,7 +1,8 @@
 $formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
-  Echo "test: muxing to $outfile..."
+  Echo "begin test: muxing to $outfile..."
   & .\muxing.exe $outfile >$null 2>&1
+  Echo "end test: muxing to $outfile..."
   if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
 }
