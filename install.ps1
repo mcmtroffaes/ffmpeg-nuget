ac9e3d14bac7183482cb03167b14758e24405a05
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-25 08:35:01 +0100

    Change log level in muxing test.

--- install.ps1
+++ install.ps1
@@ -24,3 +24,8 @@ foreach ($file in $cfiles)
   Foreach-Object { $_ -replace "unistd.h", "io.h" } |
     Set-Content $file.PSPath
 }
+
+# change log level for muxing (so test only writes to stderr on actual error)
+(Get-Content examples\muxing.c) |
+Foreach-Object {$_ -replace "if (argc", "av_log_set_level(AV_LOG_ERROR); if (argc"} |
+  Set-Content examples\muxing.c
