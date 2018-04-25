518b22e116dbe4e8c5d0af1b80b786fedbaf1a7a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-25 08:20:15 +0100

    Try to fix stderr output issue again.

--- test/test.ps1
+++ test/test.ps1
@@ -2,7 +2,15 @@ $formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
   Echo "begin test: muxing to $outfile..."
-  & .\muxing.exe $outfile 2>&1 | out-null
+  # muxing.exe writes to stderr, which powershell considers as a failure
+  # so embed in try catch to fix this
+  Try
+  {
+    & .\muxing.exe $outfile 2>&1 | out-null
+  }
+  Catch
+  {
+  }
   Echo "end test: muxing to $outfile..."
   if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
 }
