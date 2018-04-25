5e449f3c4aee11f3f20ccb1da356d7c1fefdeb5e
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-25 08:11:31 +0100

    Try to fix powershell issue.

--- test/test.ps1
+++ test/test.ps1
@@ -2,7 +2,7 @@ $formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
   Echo "begin test: muxing to $outfile..."
-  & .\muxing.exe $outfile >$null 2>&1
+  & .\muxing.exe $outfile 2>&1 | out-null
   Echo "end test: muxing to $outfile..."
   if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
 }
