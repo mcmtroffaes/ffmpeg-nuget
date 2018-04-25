261297c822464a42f077097010722a3528186400
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-25 10:29:58 +0100

    Reenable redirect stderr.

--- test/test.ps1
+++ test/test.ps1
@@ -4,7 +4,7 @@ foreach ($format in $formats) {
   Echo "muxing to $outfile..."
   # muxing.exe writes to stderr, which powershell considers as a failure
   # so execute with cmd and redirect stderr within cmd to hide from powershell
-  cmd /c ".\muxing.exe $outfile > nul"
+  cmd /c ".\muxing.exe $outfile > nul 2>&1"
   if (!(Test-Path $outfile)) {
     Echo "test failed: output file not found"
     $host.SetShouldExit(1)
