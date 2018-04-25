fe4ed8a9e1a9b85156066b9f15ab07fd0210a020
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-04-25 09:21:26 +0100

    Just signal error by printing appropriate message.

--- test/test.ps1
+++ test/test.ps1
@@ -1,11 +1,12 @@
 $formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
-  Echo "begin test: muxing to $outfile..."
+  Echo "muxing to $outfile..."
   # muxing.exe writes to stderr, which powershell considers as a failure
   # so execute with cmd and redirect stderr within cmd to hide from powershell
   cmd /c ".\muxing.exe $outfile > nul 2>&1"
-  Echo "end test: muxing to $outfile..."
-  if (!(Test-Path $outfile)) { $host.SetShouldExit(1) }
+  if (!(Test-Path $outfile)) {
+    Echo "test failed: output file not found"
+    $host.SetShouldExit(1)
+  }
 }
-Get-ChildItem . | Select Name
