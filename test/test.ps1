1d4d87bd8a45ca9aca6bb850fc542c3d53087afb
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-07-09 08:00:53 +0100

    Re-enable flac in test to see if it works again.
    Revert "flag test fails, remove for now"
    
    This reverts commit afad55fbbf45495b2c30d7155e64c522a1d9f5bf.

--- test/test.ps1
+++ test/test.ps1
@@ -1,4 +1,4 @@
-$formats = @("wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
+$formats = @("flac","wav","aac","ogg","mp4","mkv","flv","mov","avi","h264","h265")
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
   Echo "muxing to $outfile..."
